1e26c55aee82cbbcd55b97dbd3772997ed823238
frckbrice <maebrie2017@gmail.com>
2024-08-14 12:09:23 +0100

    basic setup of the project

---
+++ src/App.js
@@ -0,0 +1,47 @@
+import { useState } from "react";
+import { Routes, Route } from "react-router-dom";
+import Topbar from "./scenes/global/Topbar";
+import Sidebar from "./scenes/global/Sidebar";
+import Dashboard from "./scenes/dashboard";
+import Team from "./scenes/team";
+import Invoices from "./scenes/invoices";
+import Contacts from "./scenes/contacts";
+import Form from "./scenes/form";
+import Pie from "./scenes/pie";
+
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { ColorModeContext, useMode } from "./theme";
+import Calendar from "./scenes/calendar/calendar";
+import CreateOrder from "./scenes/orders/create_orders";
+
+function App() {
+  const [theme, colorMode] = useMode();
+  const [isSidebar, setIsSidebar] = useState(true);
+
+  return (
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <div className="app">
+          <Sidebar isSidebar={isSidebar} />
+          <main className="content">
+            <Topbar setIsSidebar={setIsSidebar} />
+            <Routes>
+              <Route path="/" element={<Dashboard />} />
+              <Route path="/team" element={<Team />} />
+              <Route path="/contacts" element={<Contacts />} />
+              <Route path="/invoices" element={<Invoices />} />
+              <Route path="/form" element={<Form />} />
+              <Route path="/create_orders" element={<CreateOrder />} />
+              <Route path="/pie" element={<Pie />} />
+              {/* <Route path="/orders" element={< />} /> */}
+              <Route path="/calendar" element={<Calendar />} />
+            </Routes>
+          </main>
+        </div>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
+  );
+}
+
+export default App;
---
+++ src/scenes/orders/create_orders.jsx
@@ -0,0 +1,121 @@
+// import React, { useEffect, useState } from "react";
+// import { useNavigate } from "react-router-dom";
+
+// const CreateOrder = () => {
+//   const toast = useToast();
+
+//   const navigate = useNavigate();
+//   const [form, setForm] = useState({
+//     userId: "",
+//     pickupDate: "",
+//     topwears: "",
+//     bottomwears: "",
+//     woolenCloths: "",
+//     others: "",
+//     serviceType: "",
+//     contactNumber: "",
+//     description: "",
+//   });
+
+//   const handleSubmit = () => {
+//     // console.log(form)
+//   };
+//   // useEffect(() => {
+//   //   if (!isAuth) {
+//   //     navigate("/login");
+//   //   }
+//   // }, [isAuth]);
+
+//   return (
+//     <Box p="1rem" w="80%" boxSizing="border-box" bg="#f7f7f7">
+//       {/* Laundry Request Form*/}
+//       <Box>
+//         <Flex gap="10px">
+//           <FormControl>
+//             <FormLabel>Select Pickup Date</FormLabel>
+//             <Input
+//               bgColor="whiteAlpha.700"
+//               onChange={(e) => setForm({ ...form, pickupDate: e.target.value })}
+//               size="sm"
+//               type="datetime-local"
+//             />
+//           </FormControl>
+//           <FormControl>
+//             <FormLabel>Topwear</FormLabel>
+//             <Input
+//               bgColor="whiteAlpha.700"
+//               onChange={(e) => setForm({ ...form, topwears: e.target.value })}
+//               size="sm"
+//               placeholder="Tshirt, Top, Shirt"
+//             />
+//           </FormControl>
+//         </Flex>
+//         <FormControl>
+//           <FormLabel>Bottomwear</FormLabel>
+//           <Input
+//             bgColor="whiteAlpha.700"
+//             onChange={(e) => setForm({ ...form, bottomwears: e.target.value })}
+//             size="sm"
+//             placeholder="Lower, jeans, Leggins"
+//           />
+//         </FormControl>
+//         <FormControl>
+//           <FormLabel>Woolen Cloth</FormLabel>
+//           <Input
+//             bgColor="whiteAlpha.700"
+//             onChange={(e) => setForm({ ...form, woolenCloths: e.target.value })}
+//             size="sm"
+//             placeholder="Woolen Cloth"
+//           />
+//         </FormControl>
+//         <FormControl>
+//           <FormLabel>Others</FormLabel>
+//           <Input
+//             bgColor="whiteAlpha.700"
+//             onChange={(e) => setForm({ ...form, others: e.target.value })}
+//             size="sm"
+//             placeholder="Others"
+//           />
+//         </FormControl>
+
+//         <FormControl>
+//           <FormLabel>Select Service Type</FormLabel>
+//           <Select
+//             bgColor="whiteAlpha.700"
+//             onChange={(e) => setForm({ ...form, serviceType: e.target.value })}
+//             placeholder="- - - - - - - -"
+//           >
+//             <option value="Fast">Fast Service</option>
+//             <option value="Regular">Regular Service</option>
+//           </Select>
+//         </FormControl>
+//         <FormControl>
+//           <FormLabel>Contact Number</FormLabel>
+//           <Input
+//             bgColor="whiteAlpha.700"
+//             onChange={(e) =>
+//               setForm({ ...form, contactNumber: e.target.value })
+//             }
+//             size="sm"
+//             type="number"
+//             placeholder="Contact Number"
+//           />
+//         </FormControl>
+//         <FormControl>
+//           <FormLabel>Description</FormLabel>
+//           <Input
+//             bgColor="whiteAlpha.700"
+//             onChange={(e) => setForm({ ...form, description: e.target.value })}
+//             size="sm"
+//             placeholder="Description(if any)"
+//           />
+//         </FormControl>
+//         <Button onClick={handleSubmit} mt="10px" colorScheme="blue" w="100%">
+//           Submit
+//         </Button>
+//       </Box>
+//     </Box>
+//   );
+// };
+
+// export default CreateOrder;
